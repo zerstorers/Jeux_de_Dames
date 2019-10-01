528a8dd36a47aaeaa1347da96315e20271cca228
zerstorers <lemaire-maxime@hotmail.fr>
2019-10-01 11:42:22 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var table = document.createElement("table")
+document.body.appendChild(table)
+
+
+
+
+for (let i = 0; i < 10; i++) {
+    var tr = document.createElement("tr")
+    table.appendChild(tr)
+    for (let j = 0; j < 10; j++) {
+        var td = document.createElement("td")
+        tr.appendChild(td)
+        td.className = "td"
+
+        if (i % 2 == j % 2) {
+            td.style.backgroundColor = ("brown")
+        }
+        else {
+            td.style.backgroundColor = ("black")
+            var pion = document.createElement("div")
+            td.appendChild(pion)
+            pion.className = "div1"
+            if (i == i[5]){
+                pion.style.backgroundColor = ("red")
+            }
+        }
+
+    }
+}
+
